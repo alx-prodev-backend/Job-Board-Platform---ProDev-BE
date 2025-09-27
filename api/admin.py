f3da83c83de63fcd092902ee50bfd1aaa38e05d6
hammad-Ibrahim <hammad.ibrahim21994@gmail.com>
2025-09-27 22:50:31 +0300

    Model registeration

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import User, Company, Profile, Skill, Job, Application
 
 # Register your models here.
+
+# the simple way to register models here
+admin.site.register(User)
+admin.site.register(Company)
+admin.site.register(Profile)
+admin.site.register(Skill)
+admin.site.register(Job)
+admin.site.register(Application)
