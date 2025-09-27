52c1640a23af024533aecb185ab8a847ccf07a0f
hammad-Ibrahim <hammad.ibrahim21994@gmail.com>
2025-09-28 01:26:12 +0300

    Urls Mapping app & project level

--- job_board_plt/urls.py
+++ job_board_plt/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+# Add this line:
+    # This tells Django that any URL starting with 'api/' should be
+    # handled by the urls.py file inside our 'api' app.
+    path('api/', include('api.urls')),
 ]
